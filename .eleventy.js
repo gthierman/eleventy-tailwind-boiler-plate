29c5cefc84a70a8255361834a053e29f9a20f80f
Geoff Thierman <gthierman@Geoffs-MacBook-Pro.local>
2022-08-13 03:24:31 -0700

    11ty tailwind boilerplate - initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+const now = String(Date.now())
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
+  eleventyConfig.addWatchTarget('./styles/tailwind.css')
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
+
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+}; 
