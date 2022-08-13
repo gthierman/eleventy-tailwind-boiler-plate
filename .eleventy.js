6385f2f0f6d3b65e5e96f073ff4bfe02d66c8804
“Geoff <gthierman@gmail.com>
2022-08-13 04:13:51 -0700

    Move alpinejs config to .eleventy.js config file

--- .eleventy.js
+++ .eleventy.js
@@ -3,16 +3,20 @@ const now = String(Date.now())
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./styles/tailwind.css')
-
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
-
   eleventyConfig.addShortcode('version', function () {
     return now
   })
 
+
+  // Configure Alpine
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
   })
 
+  // Layout Aliases
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+
+  // Global Data
+  eleventyConfig.addGlobalData("alpinejs", true);
 }; 
