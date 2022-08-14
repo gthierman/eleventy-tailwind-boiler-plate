b98a5ef038ef3ac813871f550ea0d37fb01dee0f
“Geoff <gthierman@gmail.com>
2022-08-14 13:26:11 -0700

    Move styles to src

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 const now = String(Date.now())
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
-  eleventyConfig.addWatchTarget('./styles/tailwind.css')
+  eleventyConfig.addWatchTarget('./src/styles/tailwind.config.js')
+  eleventyConfig.addWatchTarget('./src/styles/tailwind.css')
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
   eleventyConfig.addShortcode('version', function () {
     return now
